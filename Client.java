a3a2fc93f803b922df74763ed825f0118fdaf50b
Aaditree <43510617+Aaditree@users.noreply.github.com>
2020-05-31 19:57:43 +0530

    UDP

--- Client.java
+++ Client.java
@@ -1,74 +1,46 @@
-package goBackN;
-
-import java.net.*; 
-import java.io.*; 
-
-public class Client 
+import java.io.IOException; 
+import java.net.DatagramPacket; 
+import java.net.DatagramSocket; 
+import java.net.InetAddress; 
+import java.util.Scanner; 
+  
+public class Client
 { 
-	// initialize socket and input output streams 
-	private Socket socket		 = null; 
-	private DataInputStream input = null; 
-	private DataOutputStream out	 = null; 
-
-	// constructor to put ip address and port 
-	public Client(String address, int port) 
-	{ 
-		// establish a connection 
-		try
-		{ 
-			socket = new Socket(address, port); 
-			System.out.println("Connected"); 
-
-			// takes input from terminal 
-			input = new DataInputStream(System.in); 
-
-			// sends output to the socket 
-			out = new DataOutputStream(socket.getOutputStream()); 
-		} 
-		catch(UnknownHostException u) 
-		{ 
-			System.out.println(u); 
-		} 
-		catch(IOException i) 
-		{ 
-			System.out.println(i); 
-		} 
-
-		// string to read message from input 
-		String line = ""; 
-
-		// keep reading until "Over" is input 
-		while (!line.equals("Over")) 
-		{ 
-			try
-			{ 
-				line = input.readLine(); 
-				out.writeUTF(line); 
-			} 
-			catch(IOException i) 
-			{ 
-				System.out.println(i); 
-			} 
-		} 
-
-		// close the connection 
-		try
-		{ 
-			input.close(); 
-			out.close(); 
-			socket.close(); 
-		} 
-		catch(IOException i) 
-		{ 
-			System.out.println(i); 
-		} 
-	} 
-
-	public static void main(String args[]) 
-	{ 
-		Client client = new Client("127.0.0.1", 5000); 
-	} 
+    public static void main(String args[]) throws IOException 
+    { 
+        Scanner sc = new Scanner(System.in); 
+  
+        DatagramSocket ds = new DatagramSocket(); 
+  
+        InetAddress ip = InetAddress.getLocalHost(); 
+        byte buf[] = null; 
+  
+        while (true) 
+        { 
+            String inp = sc.nextLine(); 
+  
+            buf = inp.getBytes(); 
+  
+             DatagramPacket DpSend = 
+                  new DatagramPacket(buf, buf.length, ip, 2234); 
+  
+            ds.send(DpSend); 
+  
+            if (inp.equals("bye")) 
+                break; 
+            
+            buf = new byte[65535]; 
+            DatagramPacket DpReceive = 
+                                 new DatagramPacket(buf, buf.length); 
+            ds.receive(DpReceive); 
+  
+            System.out.println("Answer = " + 
+                                new String(buf,0,buf.length)); 
+        } 
+    } 
 } 
 
 
-Output: Connected
+Output:
+
+Answer = 8
--- Server.java
+++ Server.java
@@ -1,72 +1,93 @@
-package goBackN;
+import java.io.IOException; 
+import java.net.DatagramPacket; 
+import java.net.DatagramSocket; 
+import java.net.InetAddress; 
+import java.net.SocketException; 
+import java.util.StringTokenizer;
+  
 
-import java.net.*; 
-import java.io.*; 
-
-public class Server 
+public class Server
 { 
-	//initialize socket and input stream 
-	private Socket		 socket = null; 
-	private ServerSocket server = null; 
-	private DataInputStream in	 = null; 
+    public static void main(String[] args) throws IOException 
+    { 
+    	DatagramSocket ds = new DatagramSocket(2234); 
+        byte[] r = new byte[65535]; 
+        int a ,b;
+  
+        DatagramPacket DpReceive = null; 
+        DatagramPacket DpSend=null;
+        while (true) 
+        { 
+  
+            DpReceive = new DatagramPacket(r, r.length); 
+  
+            ds.receive(DpReceive); 
+            
+            String inp = new String(r, 0, r.length); 
+            
+            //To remove extra spaces. 
+            inp=inp.trim(); 
+            System.out.println("Equation Received:- " + inp); 
+  
+   
+  
+            if (inp.equals("bye")) 
+            { 
+                System.out.println("Client sent bye");
+                System.out.println(".....EXITING");
+                break; 
+            } 
+            
+            
+            // Use StringTokenizer to break the 
+            // equation into operand and operation 
+            StringTokenizer st = new StringTokenizer(inp); 
+  
+            int oprnd1 = Integer.parseInt(st.nextToken()); 
+            String operation = st.nextToken(); 
+            int oprnd2 = Integer.parseInt(st.nextToken());
+            int result=0; 
 
-	// constructor with port 
-	public Server(int port) 
-	{ 
-		// starts server and waits for a connection 
-		try
-		{ 
-			server = new ServerSocket(port); 
-			System.out.println("Server started"); 
+  switch(operation){
+  case "+": 
+                result = oprnd1 + oprnd2; 
+                break;
+  
+  case "-": 
+                result = oprnd1 - oprnd2; 
+                break;
+  
+  case "*":
+                result = oprnd1 * oprnd2; 
+                break;
+  
+  case "/":
+                result = oprnd1 / oprnd2; 
+                break;
+  }
+            System.out.println("Sending the result..."); 
+            String res = Integer.toString(result); 
+  
+                        
+                        r = res.getBytes(); 
+                        
+                        // get the port of client. 
+                        int port = DpReceive.getPort(); 
+              
+                        DpSend = new DatagramPacket(r, r.length, 
+                                      InetAddress.getLocalHost(), port); 
+                        ds.send(DpSend);
+                        
+                        r = new byte[65535]; 
 
-			System.out.println("Waiting for a client ..."); 
-
-			socket = server.accept(); 
-			System.out.println("Client accepted"); 
-
-			// takes input from the client socket 
-			in = new DataInputStream( 
-				new BufferedInputStream(socket.getInputStream())); 
-
-			String line = ""; 
-
-			// reads message from client until "Over" is sent 
-			while (!line.equals("Over")) 
-			{ 
-				try
-				{ 
-					line = in.readUTF(); 
-					System.out.println(line); 
-
-				} 
-				catch(IOException i) 
-				{ 
-					System.out.println(i); 
-				} 
-			} 
-			System.out.println("Closing connection"); 
-
-			// close connection 
-			socket.close(); 
-			in.close(); 
-		} 
-		catch(IOException i) 
-		{ 
-			System.out.println(i); 
-		} 
-	} 
-
-	public static void main(String args[]) 
-	{ 
-		Server server = new Server(5000); 
-	} 
+        } 
+    } 
+  
 } 
 
-Output: 
-Server started
-Waiting for a client ...
-Client accepted
-Hi
-Hello
-Over
-Closing connection
+
+Output:
+
+Equation Received:- 3+5
+Sending the result...
+
